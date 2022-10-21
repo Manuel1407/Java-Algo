df2e3972a31e04cf5503797a051966207fedad6c
Ikechukwu <iykeemma009@gmail.com>
2022-10-21 22:50:57 +0100

    added TernaryCondition

---
+++ src/main/java/org/ikechukwu/conditionalOperator/TernaryCondition.java
@@ -0,0 +1,9 @@
+package org.ikechukwu.conditionalOperator;
+
+public class TernaryCondition {
+    public static void main(String[] args) {
+        int age = 50;
+
+        System.out.println(age >= 50 ? "You are old" : "You are young");
+    }
+}
